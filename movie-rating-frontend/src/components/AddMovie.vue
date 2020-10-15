322f93305af74a90c197efc9e24fe8e339e887f6
beinganushasan <beinganushasan@gmail.com>
2020-10-15 16:47:27 +0545

    some css changes for  better look

--- movie-rating-frontend/src/components/AddMovie.vue
+++ movie-rating-frontend/src/components/AddMovie.vue
@@ -1,130 +1,105 @@
 <template>
-  <div id="addmovie">
-    <h1 id="textTop">Add Movie To Library</h1>
-    <button @click="increment">Increment {{ $store.state.count }}</button>
-    <div class="search">
-      <input
-        type="text"
-        placeholder="Search"
-        v-model="searchName"
-        @input="searchByName"
-        required
-      />
-      <b-button type="submit" @click="searchByName">Search</b-button>
-
-      <div
-        class="floatingRectangle"
-<<<<<<< HEAD
-        v-if="showSearchResult && searchResponseMovie.length"
-=======
-        v-if="showSearchResult && theMovieDBResponse.length"
->>>>>>> working
-      >
-        <ul>
-          <li
-            @click="fillTheForm(movie, $event)"
-            v-for="movie in theMovieDBResponse.slice(0, 5)"
-            :key="movie.id"
-          >
-            {{ movie.title }} : {{ movie.release_date }}
-          </li>
-        </ul>
-      </div>
-    </div>
-    <br />
-
-    <b-form class="my-form" @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form inline>
-        <b-form-group id="input-group-1" label-for="input-1" description="">
-          <b-form-input
-            id="input-1"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            style="width: 20em"
-            v-model="form.movieName"
-            type="text"
-            required
-            placeholder="Movie Name"
-          ></b-form-input>
-        </b-form-group>
-
-<<<<<<< HEAD
-        <b-form-input
-          id="range"
-          type="range"
-          class="rating"
-          v-model="form.rating"
-          min="0"
-          max="10"
-          step="0.1"
-        ></b-form-input>
-        {{ this.form.rating }}*
-        <!-- <b-form-group id="input-group-3" label="" label-for="input-3">
-          <b-form-select
-=======
-        <b-form-group id="input-group-3" label="" label-for="input-3">
-          <b-form-input
-            id="input-3"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            style="width: 20em"
-            v-model="form.rating"
-            type="number"
-            min="1"
-            max="10.1"
-            step="0.5"
-            required
-            placeholder="Movie Rating"
-          ></b-form-input>
-          <!-- <b-form-select
->>>>>>> working
-            id="input-3"
-            class="rating"
-            v-model="form.rating"
-            :options="ratings"
-            required
-<<<<<<< HEAD
-          ></b-form-select>
-        </b-form-group> -->
-=======
-          ></b-form-select> -->
-        </b-form-group>
->>>>>>> working
-      </b-form>
-
-      <b-form-group
-        id="input-group-2"
-        label-align="left"
-        label=""
-        label-for="input-2"
-      >
-        <b-form-textarea
-          id="input-2"
-          v-model="form.description"
+  <div class="container">
+    <div class="card bg-light">
+      <h1 id="textTop">Add To Library</h1>
+      <!-- <button @click="increment">Increment {{ $store.state.count }}</button> -->
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          v-model="searchName"
+          @input="searchByName"
           required
-          placeholder="Description"
-        ></b-form-textarea>
-      </b-form-group>
+        />
+        <b-button type="submit" @click="searchByName">Search</b-button>
 
-      <b-form-group id="input-group-4" label="Genre:">
-        <b-form-checkbox-group v-model="form.genre" id="checkboxes-4">
-          <!-- <b-form-checkbox
+        <div
+          class="floatingRectangle"
+          v-if="showSearchResult && theMovieDBResponse.length"
+        >
+          <ul>
+            <li
+              @click="fillTheForm(movie, $event)"
+              v-for="movie in theMovieDBResponse.slice(0, 5)"
+              :key="movie.id"
+            >
+              {{ movie.title }} : {{ movie.release_date }}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+
+      <b-form class="my-form" @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-container>
+          <b-row>
+            <b-col>
+              <b-form-group
+                id="input-group-1"
+                label-for="input-1"
+                description=""
+              >
+                <b-form-input
+                  id="input-1"
+                  v-model="form.movieName"
+                  type="text"
+                  required
+                  placeholder="Movie Name"
+                ></b-form-input>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group id="input-group-3" label="" label-for="input-3">
+                <b-form-input
+                  id="input-3"
+                  v-model="form.rating"
+                  type="number"
+                  min="1"
+                  max="10.1"
+                  step="0.5"
+                  required
+                  placeholder="Movie Rating"
+                ></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-row>
+        </b-container>
+
+        <b-form-group
+          id="input-group-2"
+          label-align="left"
+          label=""
+          label-for="input-2"
+        >
+          <b-form-textarea
+            id="input-2"
+            v-model="form.description"
+            required
+            placeholder="Description"
+          ></b-form-textarea>
+        </b-form-group>
+
+        <b-form-group id="input-group-4" label="Genre:">
+          <b-form-checkbox-group v-model="form.genre" id="checkboxes-4">
+            <!-- <b-form-checkbox
             v-for="genre in genres"
             value="genre.value"
             :key="genre.id"
             >{{ genre.value }}</b-form-checkbox> -->
-          <b-form-checkbox value="Action">Action</b-form-checkbox>
-          <b-form-checkbox value="Drama">Drama</b-form-checkbox>
-          <b-form-checkbox value="Romance">Romance</b-form-checkbox>
-          <b-form-checkbox value="Comedy">Comedy</b-form-checkbox>
-          <b-form-checkbox value="Horror">Horror</b-form-checkbox>
-          <b-form-checkbox value="Thriller">Thriller</b-form-checkbox>
-          <b-form-checkbox value="Si-fi">Si-fi</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
+            <b-form-checkbox value="Action">Action</b-form-checkbox>
+            <b-form-checkbox value="Drama">Drama</b-form-checkbox>
+            <b-form-checkbox value="Romance">Romance</b-form-checkbox>
+            <b-form-checkbox value="Comedy">Comedy</b-form-checkbox>
+            <b-form-checkbox value="Horror">Horror</b-form-checkbox>
+            <b-form-checkbox value="Thriller">Thriller</b-form-checkbox>
+            <b-form-checkbox value="Si-fi">Si-fi</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
 
-      <b-button type="submit" variant="primary">Add To Libary</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-
+        <b-button type="submit" variant="primary">Add To Libary</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </div>
     <div class="mt-3">
       <b-button size="sm" variant="primary" @click="showJSON"
         >Show JSON</b-button
@@ -169,23 +144,12 @@ export default {
       this.$store.commit("increment");
     },
     searchByName() {
-<<<<<<< HEAD
-      this.$axios
-        .get(
-          "https://api.themoviedb.org/3/search/movie?api_key=f69685ff175d2d4f542c2d6001185d43&page=1&query=" +
-            this.searchName
-        )
-        .then((response) => (this.searchResponseMovie = response.data.results));
-      this.showSearchResult = true;
-      // .then(this.form.movieName = this.searchResponseMovie.Title);
-=======
       if (this.searchName != null) {
         API.searchByNameTheMovieDB(this.searchName).then(
           (response) => (this.theMovieDBResponse = response.data.results)
         );
         this.showSearchResult = true;
       } else alert("Enter Search Name");
->>>>>>> working
     },
 
     onSubmit(evt) {
@@ -235,12 +199,12 @@ export default {
 }
 
 .my-form {
-  width: 50em;
+  width: 100%;
   margin: auto;
 }
 .rating {
-  width: 25em;
-  margin-right: 0em;
+  width: 100%;
+  /* margin-right: 0em; */
 }
 .floatingRectangle {
   z-index: 1;
