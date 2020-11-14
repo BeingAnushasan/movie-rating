232b43ba1f1a0d7429498e261ae1356a5085d83e
beinganushasan <beinganushasan@gmail.com>
2020-11-14 12:17:42 +0545

    uses bcrypt encoder for password encoding

--- backend/src/main/java/com/tiger/movierating/configuratons/SecurityConfigurer.java
+++ backend/src/main/java/com/tiger/movierating/configuratons/SecurityConfigurer.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -54,8 +55,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter implements 
 
     @Bean
     PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
+
 
     @Override
     @Bean
@@ -76,5 +79,3 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter implements 
     }
 
 }
-//abhiyantimilsina96@gmail.com
-//badtoseecdream1
--- backend/src/main/java/com/tiger/movierating/services/MyUserDetailsService.java
+++ backend/src/main/java/com/tiger/movierating/services/MyUserDetailsService.java
@@ -1,24 +1,29 @@
 package com.tiger.movierating.services;
 
 import com.tiger.movierating.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
-
+    @Autowired
+    PasswordEncoder passwordEncoder;
     final UserRepo userRepo;
 
     public MyUserDetailsService( UserRepo userRepo ){
         this.userRepo = userRepo;
+
     }
 
     public com.tiger.movierating.models.UserDetails.User createUser( com.tiger.movierating.models.UserDetails.User user ){
+        user.setPassword( passwordEncoder.encode( user.getPassword() ) );
         return userRepo.save( user );
     }
 
@@ -26,10 +31,10 @@ public class MyUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername( String s ) throws UsernameNotFoundException{
 
 //        return new User( "admin", "admin", new ArrayList<>() );
-        System.out.println("Loading user by username where username is: "+s);
+        System.out.println( "Loading user by username where username is: " + s );
         com.tiger.movierating.models.UserDetails.User byUsername = userRepo.findByUsername( s );
-        System.out.println("Loaded user by username is: "+byUsername);
-        return new User( byUsername.getUsername(), byUsername.getPassword(), new ArrayList<>());
+        System.out.println( "Loaded user by username is: " + byUsername );
+        return new User( byUsername.getUsername(), byUsername.getPassword(), new ArrayList<>() );
     }
 
 
